4426deeee844b8ba54cc3662235d65469832b3e2
Jc Santiago <jc.santiago0312@gmail.com>
2026-05-08 08:03:49 +0800

    added a code system

---
+++ ind.js
@@ -0,0 +1,53 @@
+function checkPassword(event) {
+    event.preventDefault();
+    const input = document.getElementById("pass").value;
+
+    switch (input) {
+        case "deb1":
+            window.location.href = "deb1.html";
+            break;
+        case "deb2":
+            window.location.href = "deb2.html";
+            break;
+        case "deb3":
+            window.location.href = "deb3.html";
+            break;
+        case "dew1":
+            window.location.href = "dew1.html";
+            break;
+        case "dew2":
+            window.location.href = "dew2.html";
+            break;
+        case "dew3":
+            window.location.href = "dew3.html";
+            break;
+        case "dew4":
+            window.location.href = "dew4.html";
+            break;
+        case "reglog":
+            window.location.href = "reglog.html";
+            break;
+        case "reprof":
+            window.location.href = "reprof.html";
+            break;  
+        case "relog1":
+            window.location.href = "relog1.html";
+            break;
+        case "relog2":
+            window.location.href = "relog1.html";
+            break;
+        case "relog3":
+            window.location.href = "relog1.html";
+            break;
+        case "relog4":
+            window.location.href = "relog1.html";
+            break;
+        default: alert("Wrong code. Please try again.");
+    }
+}
+
+window.addEventListener("pageshow", () => {
+    const input = document.getElementById("pass");
+    input.value = "";
+    input.focus();
+});
